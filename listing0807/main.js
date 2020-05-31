ca402158adf118db7cefd89306d018f57728979c
Jon <sasha2015to@gmail.com>
2020-06-01 01:48:06 +0300

    https://schoolsw3.com/html/html_responsive.php

--- listing0807/main.js
+++
@@ -1,9 +0,0 @@
-document.onmousemove = function(evt) {
-  var d = document.getElementsByTagName("div")[1];
-  var txt = "<b>Координаты курсора мыши.</b><br>";
-  txt += "Горизонтальная координата: <b>" + evt.clientX + "</b><br>";
-  txt += "Вертикальная координата: &nbsp&nbsp&nbsp<b>" + evt.clientY + "</b>";
-  d.innerHTML = txt;
-
-  // show2(evt);
-};
--- slick/slider.js
+++
@@ -1,8 +0,0 @@
-$(".regular").slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    autoplay: false,
-    autoplaySpeed: 3000,
-});
