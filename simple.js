7100ea7879f4fa773c24d148c0d1062f2f395ff3
atutby <sasha2015to@gmail.com>
2020-06-05 11:03:20 +0300

    little update you understand

--- simple.js
+++ simple.js
@@ -1,1 +1,13 @@
-simple
+let json = "{ bad json }";
+
+try {
+
+  let user = JSON.parse(json); // <-- when an error occurs...
+  alert( user.name ); // doesn't work
+
+} catch (e) {
+  // ...the execution jumps here
+  console.log( "Our apologies, the data has errors, we'll try to request it one more time." );
+  console.log( e.name );
+  console.log( e.message );
+}
