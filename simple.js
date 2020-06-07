de5a6acbf915776a13e930c788f92c946adc6682
atutby <sasha2015to@gmail.com>
2020-06-07 09:54:01 +0300

    little change simple.js and cat.html

--- simple.js
+++ simple.js
@@ -1,13 +1,9 @@
-let json = "{ bad json }";
+let json = '{ "age": 30}';
 
 try {
+	user = JSON.parse(json);
 
-  let user = JSON.parse(json); // <-- when an error occurs...
-  alert( user.name ); // doesn't work
+} catch(err) {
+	console.log("JSON Error: " + err);
 
-} catch (e) {
-  // ...the execution jumps here
-  console.log( "Our apologies, the data has errors, we'll try to request it one more time." );
-  console.log( e.name );
-  console.log( e.message );
 }
