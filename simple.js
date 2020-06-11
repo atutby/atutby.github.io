996c5468f252927d7c04d61fb213d290ad62255c
atutby <sasha2015to@gmail.com>
2020-06-11 10:19:53 +0300

    Lenka and try...catch...finally

--- simple.js
+++ simple.js
@@ -1,9 +1,26 @@
-let json = '{ "age": 30}';
+let num = +prompt("Enter a positive integer number?", 35)
+
+let diff, result;
+
+function fib(n) {
+	if( n < 0 || Math.trunc(n) != n) {
+		throw new Error("Must not be negative, and also an integer.")
+
+	}
+	return n <= 1 ? n : fib(n-1) + fib(n - 2);
+}
+
+let start = Date.now();
 
 try {
-	user = JSON.parse(json);
+	result = fib(num);
 
-} catch(err) {
-	console.log("JSON Error: " + err);
-
+} catch (e) {
+	result = 0;
+} finally {
+	diff = Date.now() - start;
 }
+
+alert(result || 'error occurred')
+
+alert( `execution took ${diff}ms` );
