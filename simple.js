24e253993627119c8913a1fcf8dea7af70078e56
Jon <sasha2015to@gmail.com>
2020-06-16 10:58:25 +0300

    custom-errors

--- simple.js
+++ simple.js
@@ -1,26 +1,16 @@
-let num = +prompt("Enter a positive integer number?", 35)
 
-let diff, result;
 
-function fib(n) {
-	if( n < 0 || Math.trunc(n) != n) {
-		throw new Error("Must not be negative, and also an integer.")
-
+class FormatError extends SyntaxError {
+	constructor(message) {
+	  super(message);
+	  this.name = "FormatError";
 	}
-	return n <= 1 ? n : fib(n-1) + fib(n - 2);
-}
-
-let start = Date.now();
-
-try {
-	result = fib(num);
-
-} catch (e) {
-	result = 0;
-} finally {
-	diff = Date.now() - start;
-}
-
-alert(result || 'error occurred')
-
-alert( `execution took ${diff}ms` );
+  }
+  
+  let err = new FormatError("ошибка форматирования");
+  
+  alert( err.message ); // ошибка форматирования
+  alert( err.name ); // FormatError
+  alert( err.stack ); // stack
+  
+  alert( err instanceof SyntaxError ); // true
