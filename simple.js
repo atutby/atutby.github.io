f4e860409b94ed2792699bae9a81307e080c8bb9
Halavach Aliaksandr <41650220+atutby@users.noreply.github.com>
2020-06-16 17:24:31 +0000

    update for 35

--- simple.js
+++ simple.js
@@ -1,16 +1,26 @@
+333let num = +prompt("Enter a positive integer number?", 35)
 
+let diff, result;
 
-class FormatError extends SyntaxError {
-	constructor(message) {
-	  super(message);
-	  this.name = "FormatError";
+function fib(n) {
+	if( n < 0 || Math.trunc(n) != n) {
+		throw new Error("Must not be negative, and also an integer.")
+
 	}
-  }
-  
-  let err = new FormatError("ошибка форматирования");
-  
-  alert( err.message ); // ошибка форматирования
-  alert( err.name ); // FormatError
-  alert( err.stack ); // stack
-  
-  alert( err instanceof SyntaxError ); // true
+	return n <= 1 ? n : fib(n-1) + fib(n - 2);
+}
+
+let start = Date.now();
+
+try {
+	result = fib(num);
+
+} catch (e) {
+	result = 0;
+} finally {
+	diff = Date.now() - start;
+}
+
+alert(result || 'error occurred')
+
+alert( `execution took ${diff}ms` );
