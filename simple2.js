36b0ebf9fe830d2991289b9fba0d4ada9660d183
atutby <sasha2015to@gmail.com>
2020-06-21 14:16:44 +0300

    thirty minutes

--- simple2.js
+++ simple2.js
@@ -1,21 +1,23 @@
-let promise = new Promise(function(resolve, reject) {
-	setTimeout(() => resolve("done!"), 1000);
-  });
-  
-  // resolve запустит первую функцию, переданную в .then
-  promise.then(
-	result => alert(result), // выведет "done!" через одну секунду
-	error => alert(error) // не будет запущена
-  );
-  
+function loadScript(src) {
+  return new Promise(function(resolve, reject) {
+    let script = document.createElement('script');
+    script.src = src;
 
-  let promise2 = new Promise(function(resolve, reject) {
-	setTimeout(() => reject(new Error("Whoops!")), 1000);
+    script.onload = () => resolve(script);
+    script.onerror = () => reject(new Error(`Ошибка загрузки скрипта ${src}`));
+
+    document.head.append(script);
   });
-  
-  // reject запустит вторую функцию, переданную в .then
-  promise2.then(
-	result => alert(result), // не будет запущена
-	error => alert(error) // выведет "Error: Whoops!" спустя одну секунду
-  );
+}
+
+
+let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
+
+promise.then(
+  script => alert(`${script.src} загружен!`),
+  error => alert(`Ошибка: ${error.message}`)
+);
+
+promise.then(script => alert('Ещё один обработчик...'));
+
 
