5cccdd1b6b4def4b18d4eacd96793670c7c9654e
atutby <sasha2015to@gmail.com>
2020-06-27 10:02:23 +0300

    callback loadScript

--- simple2.js
+++ simple2.js
@@ -1,23 +1,7 @@
 function loadScript(src) {
-  return new Promise(function(resolve, reject) {
-    let script = document.createElement('script');
-    script.src = src;
-
-    script.onload = () => resolve(script);
-    script.onerror = () => reject(new Error(`Ошибка загрузки скрипта ${src}`));
-
-    document.head.append(script);
-  });
+  let script = document.createElement('script');
+  script.src = src;
+  document.head.append(script);
 }
 
-
-let promise = loadScript("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js");
-
-promise.then(
-  script => alert(`${script.src} загружен!`),
-  error => alert(`Ошибка: ${error.message}`)
-);
-
-promise.then(script => alert('Ещё один обработчик...'));
-
-
+loadScript('simple3.js');
---
+++ simple3.js
@@ -0,0 +1,1 @@
+alert('Hello I am simple3.js');
