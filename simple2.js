fc5c0eeb88188aae044a5091cb1cd94f529b2961
atutby <sasha2015to@gmail.com>
2020-07-02 12:50:02 +0300

    Async iterators and generators

--- simple2.js
+++ simple2.js
@@ -1,13 +1,44 @@
-async function loadJson(url) {
-  let response = await fetch(url);
+let range = {
+  from: 1,
+  to: 3,
 
-  if (response.status == 200) {
-    let json = await response.json();
-    return json;
+  // for await..of calls this method once in the very beginning
+  [Symbol.asyncIterator]() {
+    // ...it returns the iterator object:
+    // onward, for await..of works only with that object,
+    // asking it for next values using next()
+    return {
+      current: this.from,
+      last: this.to,
+
+      // next() is called on each iteration by the for await..of loop
+      async next() {
+        // it should return the value as an object {done:.., value:...}
+        // (automatically wrapped into a promise by async)
+
+        // can use await inside, do async suff:
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+
+      }
+    };
   }
 
-  throw new Error(response.status);
+};
+
+function go() {
+  alert('I am here');
 }
 
-loadJson('no-such-user.json')
-  .catch(alert);
+
+const go2 = (async () => {
+
+  for await (let value of range) {
+    alert(value); // 1,2,3,4,5
+  }
+})
