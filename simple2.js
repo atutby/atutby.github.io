49c7c86a427e113f9cdfa838c6a769e8c9c1fce2
atutby <sasha2015to@gmail.com>
2020-07-04 10:20:26 +0300

    Модули, введение

--- simple2.js
+++ simple2.js
@@ -1,36 +1,4 @@
-async function* fetchCommits(repo) {
-  let url = `https://api.github.com/repos/${repo}/commits`;
+import {sayHi} from './sayHi.js';
 
-  while (url) {
-    const response = await fetch(url, { // (1)
-      headers: {'User-Agent': 'Our script'}, // GitHub требует заголовок user-agent
-    });
-
-    const body = await response.json(); // (2) ответ в формате JSON (массив коммитов)
-
-    // (3) Ссылка на следующую страницу находится в заголовках, извлекаем её
-    let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
-    nextPage = nextPage && nextPage[1];
-
-    url = nextPage;
-
-    for(let commit of body) { // (4) вернуть коммиты один за другим, до окончания страницы
-      yield commit;
-    }
-  }
-}
-
-const go2 = (async () => {
-
-  let count = 0;
-
-  for await (const commit of fetchCommits('atutby/atutby.github.io')) {
-
-    console.log(commit.commit.message);
-
-    if (++count == 100) { // остановимся на 100 коммитах
-      break;
-    }
-  }
-
-});
+alert(sayHi);
+sayHi('John');
