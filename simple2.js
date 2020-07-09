52c1cf57eeaac5890e438a8ef0ae93e208bd6cd4
atutby <sasha2015to@gmail.com>
2020-07-09 10:10:53 +0300

    Proxy и Reflect validation with set trap

--- simple2.js
+++ simple2.js
@@ -1,6 +1,21 @@
-async function load() {
-  let say = await import('./say.js');
-  say.hi();
-  say.bye();
-  say.default();
-}
+let numbers = [];
+
+numbers = new Proxy(numbers, { // (*)
+  set(target, prop, val) { // для перехвата записи свойства
+    if (typeof val == 'number') {
+      target[prop] = val;
+      return true;
+    } else {
+      return false;
+    }
+  }
+});
+
+numbers.push(1); // добавилось успешно
+numbers.push(2); // добавилось успешно
+console.log(numbers);
+console.log("Длина: " + numbers.length); // 2
+
+numbers.push('text'); // TypeError (ловушка set на прокси вернула false)
+console.log(numbers);
+console.log("Интерпретатор никогда не доходит до этой строки (из-за ошибки в строке выше)");
