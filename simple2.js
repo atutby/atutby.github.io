ea31bb3dd4cea3ad426609aea1be9b9e3b555d4f
atutby <sasha2015to@gmail.com>
2020-07-22 09:27:07 +0300

    BigInt

--- simple2.js
+++ simple2.js
@@ -1,23 +1,29 @@
-function curry(func) {
+var access = parseInt("11000", 2);
 
-  return function curried(...args) {
-    if (args.length >= func.length) {
-      return func.apply(this, args);
-    } else {
-      return function(...args2) {
-        return curried.apply(this, args.concat(args2));
-      }
-    }
-  };
+console.log( access );
 
+var access2 = access.toString(2);
+
+console.log( access2 );
+
+console.log( ~3 );
+console.log(`~n == -(n+1) Example ~3 is ${~3}`);
+
+// let a = +prompt('Enter your number ~', 3);
+// console.log(~a);
+// console.log(`~n == -(n+1) Example ~${a} is ${~a}`);
+
+console.log( 3 << 1 );
+console.log( 100 >> 2 );
+
+function isInteger(num) {
+  return (num ^ 0) === num;
 }
+console.log( isInteger(1) );
 
-function sum(a, b, c) {
-  return a + b + c;
-}
-
-let curriedSum = curry(sum);
-
-console.log( curriedSum(1, 2, 3) );
-console.log( curriedSum(1)(2,3) );
-console.log( curriedSum(1)(2)(3) );
+let bigint = 1n;
+let number = 2;
+let number2 = bigint + BigInt(number);
+console.log(number2);
+console.log(typeof number2);
+console.log(Number(bigint) + number);
