62e77a662f06fd84b7c7b95cb94c96c8070797e0
atutby <sasha2015to@gmail.com>
2020-07-23 10:14:21 +0300

    Intl

--- simple2.js
+++ simple2.js
@@ -1,29 +1,14 @@
-var access = parseInt("11000", 2);
+let date = new Date(2014, 11, 31, 12, 30, 055);
 
-console.log( access );
+let formatter = new Intl.DateTimeFormat("ru");
+alert( formatter.format(date) );
 
-var access2 = access.toString(2);
+formatter = new Intl.DateTimeFormat("en-US");
+alert( formatter.format(date) );
 
-console.log( access2 );
-
-console.log( ~3 );
-console.log(`~n == -(n+1) Example ~3 is ${~3}`);
-
-// let a = +prompt('Enter your number ~', 3);
-// console.log(~a);
-// console.log(`~n == -(n+1) Example ~${a} is ${~a}`);
-
-console.log( 3 << 1 );
-console.log( 100 >> 2 );
-
-function isInteger(num) {
-  return (num ^ 0) === num;
-}
-console.log( isInteger(1) );
-
-let bigint = 1n;
-let number = 2;
-let number2 = bigint + BigInt(number);
-console.log(number2);
-console.log(typeof number2);
-console.log(Number(bigint) + number);
+formatter = new Intl.DateTimeFormat("ru", {
+  hour: "numeric",
+  minute: "numeric",
+  second: "numeric",
+});
+alert( formatter.format(date) );
