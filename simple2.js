668da1b9967c7b4091f55438ba7ae8874cbea653
atutby <sasha2015to@gmail.com>
2020-07-31 09:43:11 +0300

    basic-dom-node-properties outerHTML

--- simple2.js
+++ simple2.js
@@ -1,23 +1,8 @@
+let div = document.querySelector('div');
 
-let elements = document.querySelectorAll('ul > li:last-child');
+div.outerHTML = '<p>New element</p>';
 
-for (let elem of elements) {
-  console.log(elem.innerHTML);
-}
+alert( div.outerHTML );
 
-for (let elem of document.body.children) {
-  if (elem.matches('a[href$="zip"]')) {
-    console.log("The archive reference: " + elem.href)
-  }
-}
-
-let chapter = document.querySelector('.chapter');
-
-console.log(chapter.closest('.book'));
-console.log(chapter.closest('.contents'));
-console.log(chapter.closest('h1'));
-
-let inputs = table.getElementsByTagName('input')
-for (let input of inputs) {
-  console.log( input.value + ': ' + input.checked );
-}
+div = document.querySelector('p');
+alert( div.outerHTML );
