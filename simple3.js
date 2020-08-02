1ac7bafcc476c7cf8309ad58688ed8f6f061e734
atutby <sasha2015to@gmail.com>
2020-08-02 09:28:33 +0300

    plus

--- simple3.js
+++ simple3.js
@@ -1,1 +1,13 @@
-alert('Hello I am simple3.js');
+let table = document.getElementById('age-table');
+console.log( table );
+
+table.getElementsByTagName('label');
+// or
+document.querySelectorAll('#age-table label');
+
+// 3.
+table.rows[0].cells[0];
+// or
+table.getElementsByTagName('td')[0];
+// or
+table.querySelector('td');
