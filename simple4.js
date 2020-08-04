c1f632a18d4fc5fc08dd866c06591e09b3356b7c
atutby <sasha2015to@gmail.com>
2020-08-04 12:29:02 +0300

    dom-attributes-and-properties#tasks

--- simple4.js
+++ simple4.js
@@ -2,7 +2,7 @@ console.log( elem.getAttribute("About") ); // 'Elephant'
 
 elem.setAttribute('Test', 123);
 
-alert( elem.outerHTML );
+console.log( elem.outerHTML );
 
 for (let attr of elem.attributes) {
     console.log( `${attr.name} = ${attr.value}` );
