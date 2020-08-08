ed87a4714f81c209ecf0239db2f829e7c85369ab
atutby <sasha2015to@gmail.com>
2020-08-08 10:28:29 +0300

    modifying-document(DocumentFragment)

--- simple5.js
+++ simple5.js
@@ -5,4 +5,24 @@ div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
 let simpleDiv = document.querySelector('.simpleDiv');
 simpleDiv.append(div);
 
-setTimeout(() => div.remove(), 2000);
+// setTimeout(() => div.remove(), 2000);
+
+second.after(first);
+
+let div2 = div.cloneNode(true);
+div2.querySelector('strong').innerHTML = 'Bye there!';
+
+div.after(div2);
+
+function getListContent() {
+    let fragment = new DocumentFragment();
+
+    for(let i=1; i<=3; i++) {
+        let li = document.createElement('li');
+        li.append(i);
+        fragment.append(li);
+    }
+
+    return fragment;
+}
+ul.append(getListContent());
