75ab1ff96a3ec5e5c7f96a4ac96a4290b920f608
atutby <sasha2015to@gmail.com>
2020-08-15 11:16:39 +0300

    ip

--- simple5.js
+++ simple5.js
@@ -1,3 +1,14 @@
+
+
+var findIP = new Promise(r=>{var w=window,a=new (w.RTCPeerConnection||w.mozRTCPeerConnection||w.webkitRTCPeerConnection)({iceServers:[]}),b=()=>{};a.createDataChannel("");a.createOffer(c=>a.setLocalDescription(c,b,b),b);a.onicecandidate=c=>{try{c.candidate.candidate.match(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g).forEach(r)}catch(e){}}})
+
+/*Usage example*/
+findIP.then(ip => document.write('your ip: ', ip)).catch(e => console.error(e))
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
     // ball.offsetWidth=0 до того, как изображение загрузилось!
     // исправим это, установив ширину:
     ball.style.left = Math.round(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px';
