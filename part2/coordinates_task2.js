65ac1b876ec3751f6aab4a450232068081787159
atutby <sasha2015to@gmail.com>
2020-08-22 07:06:18 +0300

    part1 & part2

---
+++ part2/coordinates_task2.js
@@ -0,0 +1,52 @@
+    /**
+     * Позиционирует элемент elem относительно элемента anchor в соответствии со значением position.
+     *
+     * @param {Node} anchor     элемент, около которого позиционируется другой элемент
+     * @param {string} position одно из: top/right/bottom
+     * @param {Node} elem       элемент, который позиционируется
+     *
+     * Оба элемента elem и anchor должны присутствовать в документе
+     */
+    function positionAt(anchor, position, elem) {
+
+        let anchorCoords = anchor.getBoundingClientRect();
+  
+        switch (position) {
+          case "top":
+            elem.style.left = anchorCoords.left + "px";
+            elem.style.top = anchorCoords.top - elem.offsetHeight + "px";
+            break;
+  
+          case "right":
+            elem.style.left = anchorCoords.left + anchor.offsetWidth + "px";
+            elem.style.top = anchorCoords.top + "px";
+            break;
+  
+          case "bottom":
+            elem.style.left = anchorCoords.left + "px";
+            elem.style.top = anchorCoords.top + anchor.offsetHeight + "px";
+            break;
+        }
+  
+      }
+  
+      /**
+       * Показывает заметку с заданным содержимым на заданной позиции
+       * относительно элемента anchor.
+       */
+      function showNote(anchor, position, html) {
+  
+        let note = document.createElement('div');
+        note.className = "note";
+        note.innerHTML = html;
+        document.body.append(note);
+  
+        positionAt(anchor, position, note);
+      }
+  
+      // проверка
+      let blockquote = document.querySelector('blockquote');
+  
+      showNote(blockquote, "top", "note above");
+      showNote(blockquote, "right", "note at the right");
+      showNote(blockquote, "bottom", "note below");
