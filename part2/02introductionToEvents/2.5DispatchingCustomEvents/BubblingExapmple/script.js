359e264f3dffb23b26721e19b1ef1f59aca1da80
atutby <sasha2015to@gmail.com>
2020-08-28 11:53:16 +0300

    2.5DispatchingCustomEvents

---
+++ part2/02introductionToEvents/2.5DispatchingCustomEvents/BubblingExapmple/script.js
@@ -0,0 +1,20 @@
+function hide() {
+    let event = new CustomEvent("hide", {
+        cancelable: true
+    });
+    if (!rabbit.dispatchEvent(event)) {
+        alert('The action was prevented by a handler')
+    } else {
+        rabbit.hidden = true;
+    }
+}
+
+function show() {
+    rabbit.hidden = false;
+}
+
+rabbit.addEventListener('hide', function(event) {
+    if (confirm("Call preventDefault?")) {
+        event.preventDefault();
+    }
+})G
---
+++ record/README.md
@@ -0,0 +1,11 @@
+#  Sample Code for HTML5 MediaRecorder API usage
+
+-------
+
+### [demo](https://mido22.github.io/MediaRecorder-sample/)
+
+-------
+
+### Works on:
+* Chrome: v47+
+* Firefox: v25+
