a7d5764076d386e50428048c65c6380e19649277
atutby <sasha2015to@gmail.com>
2020-09-07 16:24:51 +0300

    4.2.1

--- part2/04formsControls/4.2focusingFocusAndBlur/4.2.0/script.js
+++ part2/04formsControls/4.2focusingFocusAndBlur/4.2.0/script.js
@@ -4,17 +4,23 @@
     let log = console.log.bind(console);
 
     input.onblur = function() {
-        if (!input.value.includes('@')) {
-            input.classList.add('invalid');
+        if (!this.value.includes('@')) {
+            this.classList.add('invalid');
+            this.classList.add('error');
+            // input.focus();
             error.innerHTML = 'Пожалуйста, введите правильный email.'
+        } else {
+            this.classList.remove('error');
         }
     };
 
+    let focus = 0;
     input.onfocus = function() {
         if (this.classList.contains('invalid')) {
             this.classList.remove('invalid');
-            error.innerHTML = "";
         }
+        error.innerHTML = "";
+        log(focus++)
     };
     
 })();
