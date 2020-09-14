be34424e8a684ea62797e36df087a767dc90fad7
atutby <sasha2015to@gmail.com>
2020-09-14 11:20:24 +0300

    6.3EventLoopMicrotasksAndMacrotasks/6.3.0count()

---
+++ part2/06Miscellaneous/6.3EventLoopMicrotasksAndMacrotasks/6.3.0/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+;(function() {
+    let log = console.log.bind(console);
+//your code here...
+
+let i = 0;
+
+let start = Date.now();
+
+function count() {
+    for (let j = 0; j < 1e9; j++) {
+        i++;
+    }
+
+    alert("Done in " + (Date.now() - start) + 'ms');
+}
+count();
+})();
