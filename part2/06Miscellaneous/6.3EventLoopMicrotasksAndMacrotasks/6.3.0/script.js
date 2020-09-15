36a7e34b2fc6dd676d9e2844a9a4e5e8166c7cb3
atutby <sasha2015to@gmail.com>
2020-09-15 22:18:43 +0300

    part3

--- part2/06Miscellaneous/6.3EventLoopMicrotasksAndMacrotasks/6.3.0/script.js
+++ part2/06Miscellaneous/6.3EventLoopMicrotasksAndMacrotasks/6.3.0/script.js
@@ -1,31 +1,19 @@
 "use strict";
+let log = console.log.bind(console);
 
 ;(function() {
-    let log = console.log.bind(console);
-//your code here...
+    let i = 0;
 
-let i = 0;
+    function count() {
+        do {
+            i++;
+            progress.innerHTML = i;
+        } while (i % 1e3 != 0);
 
-let start = Date.now();
-
-function count() {
-
-    if (i < 1e9 - 1e6) {
-        setTimeout(count);
+        if (i < 1e7) {
+            setTimeout(count);
+        }
     }
 
-    do {
-        i++;
-    } while (i % 1e6 != 0);
-
-    if (i == 1e9) {
-        alert("Done in " + (Date.now() - start) + 'ms');
-    }
-    
-}
-
-const button = document.querySelector('button');
-
-button.addEventListener('click', count);
-
+    count();
 })();
