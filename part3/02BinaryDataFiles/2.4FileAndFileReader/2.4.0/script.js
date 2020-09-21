9b1cb3ef419be14e52f236babfc03d9d0f45a4e0
atutby <sasha2015to@gmail.com>
2020-09-21 10:58:13 +0300

    fetch

--- part3/02BinaryDataFiles/2.4FileAndFileReader/2.4.0/script.js
+++ part3/02BinaryDataFiles/2.4FileAndFileReader/2.4.0/script.js
@@ -5,6 +5,10 @@ function rndNum(from, to) {
     return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
+for (let i = 0; i<6; i++) {
+    log(rndNum(1, 6));
+}
+
 ;(function() {
 //your code here...
 
