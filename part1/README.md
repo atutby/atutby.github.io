bb0638078f7ac15c8a0c09a65be659f24b83083b
atutby <sasha2015to@gmail.com>
2020-10-25 20:35:50 +0300

    part1

--- part1/README.md
+++
@@ -1,5 +0,0 @@
-Открыть сайт в интернете<br>
-Open the site in the browser<br>
-https://atutby.github.io<br>
-
-Нажать на кнопку
--- part1/simple2.js
+++
@@ -1,8 +0,0 @@
-let div = document.querySelector('div');
-
-div.outerHTML = '<p>New element</p>';
-
-alert( div.outerHTML );
-
-div = document.querySelector('p');
-alert( div.outerHTML );
