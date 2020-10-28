51957944111e5d08f29fefda7d0c723aa07b9368
atutby <sasha2015to@gmail.com>
2020-10-28 11:55:32 +0300

    7.11CapturingGroups

--- part3/07RegularExpressions/7.11CapturingGroups/script.js
+++ part3/07RegularExpressions/7.11CapturingGroups/script.js
@@ -2,10 +2,20 @@
 let log = console.log.bind(console);
 
 function rndNum(from, to) {
-    return Math.floor(Math.random() * (to - from + 1) + from);
+	return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
-;(function() {
-//your code here...
+(function () {
+	//your code here...
+	let str = "Gogogo John!"
+
+	// ?: исключает go из запоминания
+	let regexp = /(?:go)+ (\w+)/i
+
+	let result = str.match(regexp);
+
+	log(result[0])
+	log(result[1]);
+	log(result.length)
 
 })();
