a35e0e717bb638efaceea1e914b22f6c20e5ba65
atutby <sasha2015to@gmail.com>
2020-10-29 19:48:12 +0300

    svipas.prettier-plus

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,30 +1,17 @@
-const mysql = require("mysql2")
+import http from 'http';
 
-const connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	database: "usersdb2",
-	password: "oo4Ax4YX3AVa"
+http.createServer(function (request, response) {
+
+}).listen(3000);
+
+const fs = require("fs");
+
+let writeableStream = fs.createWriteStream("hello.txt")
+writeableStream.write("Привет Мир!")
+writeableStream.write("Продолжение записи \n")
+writeableStream.end("Завершение записи")
+let readableStream = fs.createReadStream("hello.txt", "utf8")
+
+readableStream.on("data", function (chunk) {
+	console.log(`chunk`);
 })
-
-const users = [
-	["Bob", 22],
-	["Alice", 25],
-	["Kate", 28]
-]
-const sql2 = `INSERT INTO users(name, age) VALUES ?`;
-const sql3 = `SELECT * FROM users`;
-const sql4 = `SELECT * FROM users WHERE name=? AND age=?`
-const filter = ["Sam", 31];
-
-const sql5 = `UPDATE users SET age=? WHERE name=?`;
-const data = [34, "Sam"]
-
-const sql6 = `DELETE FROM users WHERE name=?`
-const data2 = ["Sam"]
-
-connection.query(sql6, data2, function(err, results) {
-	if(err) console.log(err);
-	console.log(results);
-})
-connection.end()
