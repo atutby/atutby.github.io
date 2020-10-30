7ca2cf12176a3441245c9e4b132538db940159e6
atutby <sasha2015to@gmail.com>
2020-10-31 01:14:04 +0300

    index2.html

--- EnglishCat/js/video12.js
+++ EnglishCat/js/video12.js
@@ -1,14 +1,22 @@
-var IMG = document.querySelectorAll("#video12 img"),
+const IMG = document.querySelectorAll("#video12 img"),
   IFRAME = document.querySelector("#video12 iframe");
-for (var i = 0; i < IMG.length; i++) {
+for (let i = 0; i < IMG.length; i++) {
   IMG[i].onclick = function () {
-    var idIMG = this.src.replace(
+    let idIMG = this.src.replace(
       /http...img.youtube.com.vi.([\s\S]*?).1.jpg/g,
       "$1"
     );
-    IFRAME.src = "https://www.youtube.com/embed/" + idIMG + "?autoplay=1";
-    console.log(IFRAME.src);
+    IFRAME.src = "https://www.youtube.com/embed/" + idIMG + "?rel=0&autoplay=1";
+    if (this.dataset.end)
+      IFRAME.src = IFRAME.src.replace(
+        /([\s\S]*)/g,
+        "$1&end=" + this.dataset.end
+      );
+    if (this.dataset.start)
+      IFRAME.src = IFRAME.src.replace(
+        /([\s\S]*)/g,
+        "$1&start=" + this.dataset.start
+      );
     this.style.backgroundColor = "#555";
-    // alert(`click ${idIMG}`);
   };
 }
