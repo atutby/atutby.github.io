d2c80e6a983076898f4beec92249529f3417e580
atutby <sasha2015to@gmail.com>
2020-10-30 12:11:15 +0300

    7.11CapturingGroups

--- part3/07RegularExpressions/7.11CapturingGroups/script.js
+++ part3/07RegularExpressions/7.11CapturingGroups/script.js
@@ -2,20 +2,28 @@
 let log = console.log.bind(console);
 
 function rndNum(from, to) {
-	return Math.floor(Math.random() * (to - from + 1) + from);
+  return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
 (function () {
-	//your code here...
-	let str = "Gogogo John!"
+  //your code here...
+  let str = "Gogogo John!";
 
-	// ?: исключает go из запоминания
-	let regexp = /(?:go)+ (\w+)/i
+  // ?: исключает go из запоминания
+  let regexp = /(?:go)+ (\w+)/i;
 
-	let result = str.match(regexp);
+  let result = str.match(regexp);
 
-	log(result[0])
-	log(result[1]);
-	log(result.length)
+  log(result[0]);
+  log(result[1]);
+  log(result.length);
+})();
 
+(function () {
+  let results = "<h1> <h2>".matchAll(/<(.*?)>/gi);
+  console.log(results);
+  console.log(results[0]);
+  results = Array.from(results);
+  console.log(results[0]);
+  console.log(results[1]);
 })();
