094057b807b594cb584aa15a9a9d8f0a67eb57ea
atutby <sasha2015to@gmail.com>
2020-10-30 14:39:58 +0300

    index3.html

--- EnglishCat/js/video12.js
+++ EnglishCat/js/video12.js
@@ -1,5 +1,28 @@
-var IMG2 = document.querySelectorAll(".video12 img"),
-  IFRAME2 = document.querySelector(".video12 iframe");
+var IMG = document.querySelectorAll("#video12 img"),
+  IFRAME = document.querySelector("#video12 iframe");
+for (var i = 0; i < IMG.length; i++) {
+  IMG[i].onclick = function () {
+    var idIMG = this.src.replace(
+      /http...img.youtube.com.vi.([\s\S]*?).1.jpg/g,
+      "$1"
+    );
+    IFRAME.src = "http://www.youtube.com/embed/" + idIMG + "?rel=0&autoplay=1";
+    if (this.dataset.end)
+      IFRAME.src = IFRAME.src.replace(
+        /([\s\S]*)/g,
+        "$1&end=" + this.dataset.end
+      );
+    if (this.dataset.start)
+      IFRAME.src = IFRAME.src.replace(
+        /([\s\S]*)/g,
+        "$1&start=" + this.dataset.start
+      );
+    this.style.backgroundColor = "#555";
+  };
+}
+
+var IMG2 = document.querySelectorAll("#video13 img"),
+  IFRAME2 = document.querySelector("#video13 iframe");
 for (var i = 0; i < IMG2.length; i++) {
   IMG2[i].onclick = function () {
     var idIMG = this.src.replace(
