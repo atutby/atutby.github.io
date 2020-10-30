d95e56e9ba68a884709fed42cd15092f6f1f55e2
atutby <sasha2015to@gmail.com>
2020-10-31 00:04:37 +0300

    JAVASCRIPT28

--- EnglishCat/js/video12.js
+++ EnglishCat/js/video12.js
@@ -20,26 +20,3 @@ for (var i = 0; i < IMG.length; i++) {
     this.style.backgroundColor = "#555";
   };
 }
-
-// var IMG2 = document.querySelectorAll("#video13 img"),
-//   IFRAME2 = document.querySelector("#video13 iframe");
-// for (var i = 0; i < IMG2.length; i++) {
-//   IMG2[i].onclick = function () {
-//     var idIMG = this.src.replace(
-//       /http...img.youtube.com.vi.([\s\S]*?).1.jpg/g,
-//       "$1"
-//     );
-//     IFRAME2.src = "http://www.youtube.com/embed/" + idIMG + "?rel=0&autoplay=1";
-//     if (this.dataset.end)
-//       IFRAME2.src = IFRAME2.src.replace(
-//         /([\s\S]*)/g,
-//         "$1&end=" + this.dataset.end
-//       );
-//     if (this.dataset.start)
-//       IFRAME2.src = IFRAME2.src.replace(
-//         /([\s\S]*)/g,
-//         "$1&start=" + this.dataset.start
-//       );
-//     this.style.backgroundColor = "#555";
-//   };
-// }
