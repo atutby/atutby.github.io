2e1c4effcbfce385e7888ba7b47316ad4d5414bc
atutby <sasha2015to@gmail.com>
2020-10-30 11:06:28 +0300

    nodeapp

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,109 +1,20 @@
-const Sequelize = require("sequelize");
 const express = require("express");
-const bodyParser = require("body-parser");
-
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-// определяем объект Sequelize
-const sequelize = new Sequelize("usersdb", "root", "oo4Ax4YX3AVa", {
-  dialect: "mysql",
-  host: "localhost",
-  define: {
-    timestamps: false,
-  },
-});
-
-// определяем модель User
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  age: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-  },
-});
-
 app.set("view engine", "hbs");
+app.set("views", "templates");
 
-// синхронизация с бд, после успшной синхронизации запускаем сервер
-sequelize
-  .sync()
-  .then(() => {
-    app.listen(3000, function () {
-      console.log("Сервер ожидает подключения...");
-    });
-  })
-  .catch((err) => console.log(err));
-
-// получение данных
-app.get("/", function (req, res) {
-  User.findAll({ raw: true })
-    .then((data) => {
-      res.render("index.hbs", {
-        users: data,
-      });
-    })
-    .catch((err) => console.log(err));
+app.use("/contact", function (request, response) {
+  response.render("contact.hbs", {
+    title: "Мои контакты",
+    emailsVisible: true,
+    emails: ["gavgav@mail.ru", "miaow@mail.ru"],
+    phone: "+123412342134",
+  });
+});
+app.use("/", function (request, response) {
+  response.end("Главная станица Main page");
 });
 
-app.get("/create", function (req, res) {
-  res.render("create.hbs");
-});
+app.listen(3000);
 
-// добавление данных
-app.post("/create", urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-
-  const username = req.body.name;
-  const userage = req.body.age;
-  User.create({ name: username, age: userage })
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
-});
-
-// получаем объект по id для редактирования
-app.get("/edit/:id", function (req, res) {
-  const userid = req.params.id;
-  User.findAll({ where: { id: userid }, raw: true })
-    .then((data) => {
-      res.render("edit.hbs", {
-        user: data[0],
-      });
-    })
-    .catch((err) => console.log(err));
-});
-
-// обновление данных в БД
-app.post("/edit", urlencodedParser, function (req, res) {
-  if (!req.body) return res.sendStatus(400);
-
-  const username = req.body.name;
-  const userage = req.body.age;
-  const userid = req.body.id;
-  User.update({ name: username, age: userage }, { where: { id: userid } })
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
-});
-
-// удаление данных
-app.post("/delete/:id", function (req, res) {
-  const userid = req.params.id;
-  User.destroy({ where: { id: userid } })
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch((err) => console.log(err));
-});
+console.log("http://localhost:3000");
