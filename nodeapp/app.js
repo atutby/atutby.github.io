5e43b69acc87f375de8cfb8863413b5d054849b9
atutby <sasha2015to@gmail.com>
2020-11-01 00:24:09 +0300

    Handlebars 3

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const expressHbs = require("express-handlebars");
 const hbs = require("hbs");
 const app = express();
 
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts",
+    defaultLayout: "layout",
+    extname: "hbs",
+  })
+);
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials");
 
@@ -16,4 +25,6 @@ app.use("/contact", function (request, response) {
 app.use("/", function (request, response) {
   response.render("home.hbs");
 });
+
+console.log("http://localhost:3000");
 app.listen(3000);
