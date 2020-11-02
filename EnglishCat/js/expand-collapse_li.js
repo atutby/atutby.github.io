f52e3b63574299bf387b345ae18e2526d15424e8
atutby <sasha2015to@gmail.com>
2020-11-02 12:00:44 +0300

    EnglishCat

---
+++ EnglishCat/js/expand-collapse_li.js
@@ -0,0 +1,8 @@
+//Развернуть/Свернуть список ссылок на js?  --- Expand / collapse the list of js links?
+//https://jsfiddle.net/sasaha2015/me8y97u0/5/
+// https://qna.habr.com/q/590846
+
+document.getElementsByClassName("control")[0].onclick = function () {
+  console.log("hren");
+  document.getElementsByClassName("crossing")[0].classList.toggle("expand");
+};
