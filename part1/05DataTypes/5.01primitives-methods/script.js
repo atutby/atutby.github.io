733bce2b117532bd50c7ce7ef33b84714e408c82
atutby <sasha2015to@gmail.com>
2021-01-01 15:30:54 +0300

    5.01primitives-methods

---
+++ part1/05DataTypes/5.01primitives-methods/script.js
@@ -0,0 +1,10 @@
+"use strict";
+let log = console.log.bind(console);
+
+function rndNum(from, to) {
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+(function () {
+  //your code here...
+})();
