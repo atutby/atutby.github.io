ce21123aa8eb1906a1299806a4857dbd11790827
atutby <sasha2015to@gmail.com>
2021-04-16 17:07:24 +0300

    6.3VariableScopeClouser_END

--- part1/06AdvancedWorkingWithFunctions/6.3VariableScopeClouser/script.js
+++ part1/06AdvancedWorkingWithFunctions/6.3VariableScopeClouser/script.js
@@ -5,6 +5,41 @@ function rndNum(from, to) {
   return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
-(function () {
-  //your code here...
-})();
+function f() {
+  let count = 0;
+
+  let obj = {
+    go: function () {
+      count++;
+    },
+    show: function () {
+      return count;
+    },
+  };
+
+  return obj;
+}
+
+let message = f();
+message.go();
+message.go();
+message.go();
+message.go();
+message.go();
+
+log(message.show());
+
+{
+  let localVariable = "localVariable";
+  log(localVariable);
+}
+
+{
+  let localVariable = "localVariable";
+  log(localVariable);
+}
+
+for (let i = 1; i <= 10; i++) {
+	if (i === 4) continue
+	log(i);
+}
